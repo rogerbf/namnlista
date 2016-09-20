27f13766d2fb99fe2d525f72d3cd60b4d2acb6ff
rogerbf <roger@bergfalk.nu>
2016-09-20 11:17:21 +0200

    renamed

---
+++ src/lib/svenskaakademien.js
@@ -0,0 +1,16 @@
+const xray = require(`./xray.js`)
+
+const url = `http://www.svenskaakademien.se/`
+
+const scrape = xray(url, `.view-name-day`, {
+  main: xray(`.name_day__main-name`, [`.name_day__name | trim`]),
+  alternative: xray(`.name_day__not-main-name`, [`.name_day__name | trim`])
+})
+
+module.exports = () =>
+  new Promise((resolve, reject) => {
+    scrape((err, names) => {
+      if (err) reject(err)
+      resolve(names)
+    })
+  })
