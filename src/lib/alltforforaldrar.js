714656ecc871db956dc335ddaf9b4b479d23f42c
rogerbf <roger@bergfalk.nu>
2016-09-20 11:18:04 +0200

    added scraper for alltforforaldrar

---
+++ src/lib/alltforforaldrar.js
@@ -0,0 +1,13 @@
+const xray = require(`./xray.js`)
+
+const url = `http://svenskanamn.alltforforaldrar.se/namnsdag`
+
+const scrape = xray(url, `.nameday-today`, ['a | trim'])
+
+module.exports = () =>
+  new Promise((resolve, reject) => {
+    scrape((err, names) => {
+      if (err) reject(err)
+      resolve({ main: names })
+    })
+  })
