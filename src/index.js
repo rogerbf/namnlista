02c41b1956c272296df97b6e791dd7892a3af3f4
rogerbf <roger@bergfalk.nu>
2016-09-20 14:22:01 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
-const sites = {
-  svenskaakademien_se: require(`./lib/svenskaakademien_se.js`),
-  alltforforaldrar_se: require(`./lib/alltforforaldrar_se.js`),
-  dagensnamn_nu: require(`./lib/dagensnamn_nu.js`),
-  dagensnamnsdag_nu: require(`./lib/dagensnamnsdag_nu.js`),
-  namnsdag_nu: require(`./lib/namnsdag_nu.js`),
-  namnsdag_eu: require(`./lib/namnsdag_eu.js`)
-}
+const sites = require(`./lib/sites.js`)
 
 module.exports = {
   today: () => {},
   all: () => {}
 }
 
-sites.namnsdag_eu().then(r => console.log(r))
+// sites.namnsdag_eu().then(r => console.log(r))
 
 // Promise.all(Object.keys(sites).map(site => sites[site]()))
 //   .then(r => console.log(r))
---
+++ src/lib/sites.js
@@ -0,0 +1,8 @@
+module.exports = {
+  svenskaakademien_se: require(`./lib/svenskaakademien_se.js`),
+  alltforforaldrar_se: require(`./lib/alltforforaldrar_se.js`),
+  dagensnamn_nu: require(`./lib/dagensnamn_nu.js`),
+  dagensnamnsdag_nu: require(`./lib/dagensnamnsdag_nu.js`),
+  namnsdag_nu: require(`./lib/namnsdag_nu.js`),
+  namnsdag_eu: require(`./lib/namnsdag_eu.js`)
+}
