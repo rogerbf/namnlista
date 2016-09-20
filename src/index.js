5c7c1abb6df9522b15dd88054063bd5f3421706c
rogerbf <roger@bergfalk.nu>
2016-09-20 11:57:50 +0200

    added dagensnamn.nu

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const svenskaakademien = require('./lib/svenskaakademien.js')
 const alltforforaldrar = require('./lib/alltforforaldrar.js')
 const dagensnamn = require('./lib/dagensnamn.js')
+const dagensnamnsdag = require('./lib/dagensnamnsdag.js')
 
 module.exports = {}
 
 // Promise.all([svenskaakademien(), alltforforaldrar()])
 //   .then(values => console.log(values))
 
-// dagensnamn().then(r => console.log(r))
+// dagensnamnsdag().then(r => console.log(r))
