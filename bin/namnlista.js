a277924b92254fc814cf6a6765189e9ce826efc2
rogerbf <roger@bergfalk.nu>
2017-09-06 23:10:20 +0200

    lookup date

--- bin/namnlista.js
+++ bin/namnlista.js
@@ -3,28 +3,56 @@ const lookupNameday = require(`../`)
 const { version } = require(`../package`)
 const log = console.log
 
+const logNames = ({ names, all }) => {
+  const { included, alternative } = names.reduce(
+    (sorted, { included, name }) =>
+      included
+        ? Object.assign({}, sorted, { included: [ ...sorted.included, name ] })
+        : Object.assign({}, sorted, {
+          alternative: [ ...sorted.alternative, name ]
+        }),
+    { included: [], alternative: [] }
+  )
+
+  const defaultOutput = `Names: ${included.join(`, `)}`
+
+  all && alternative.length > 0
+    ? log(defaultOutput.concat(` (Alternative: ${alternative.join(`, `)})`))
+    : log(defaultOutput)
+}
+
 commander.version(version)
 
 commander
   .command(`today`)
-  .option(`-a, --all,`, `Include deprecated names`)
-  .action(command => {
-    const names = lookupNameday(new Date())
-    const { included, alternative } = names.reduce(
-      (sorted, { included, name }) =>
-        included
-          ? Object.assign({}, sorted, { included: [ ...sorted.included, name ] })
-          : Object.assign({}, sorted, {
-            alternative: [ ...sorted.alternative, name ]
-          }),
-      { included: [], alternative: [] }
-    )
+  .option(`-a, --all`, `Include deprecated names`)
+  .action(({ all }) => {
+    logNames({
+      names: lookupNameday(new Date()),
+      all
+    })
+  })
 
-    const defaultOutput = `Todays names: ${included.join(`, `)}`
-
-    command.all
-      ? log(defaultOutput.concat(` (Alternative: ${alternative.join(`, `)})`))
-      : log(defaultOutput)
+commander
+  .command(`date <month> <date>`)
+  .option(`-a, --all`, `Include deprecated names`)
+  .action((month, date, { all }) => {
+    if (month < 1 || month > 12) {
+      log(`<month> is out of range`)
+      return
+    }
+    if (date < 1 || date > 31) {
+      log(`<date> is out of range`)
+      return
+    }
+    if ((new Date(`${month}-1`)).getMonth() !== (new Date(`${month}-${date}`)).getMonth()) {
+      log(`<date> is out of range`)
+    } else {
+      logNames({
+        names: lookupNameday(new Date(`${month}-${date}`)),
+        all
+      })
+    }
   })
 
 process.argv.slice(2).length === 0
