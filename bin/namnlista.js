bd6da6ebbd9e212f0f2561d51c7d3ce81717e207
rogerbf <roger@bergfalk.nu>
2017-09-06 22:31:27 +0200

    command today with option all

--- bin/namnlista.js
+++ bin/namnlista.js
@@ -1,21 +1,31 @@
 const commander = require(`commander`)
 const lookupNameday = require(`../`)
 const { version } = require(`../package`)
+const log = console.log
 
 commander.version(version)
 
-commander.command(`today`).action(() => {
-  const names = lookupNameday(new Date())
-  const { included, alternative } = names.reduce(
-    (sorted, { included, name }) =>
-      included
-        ? Object.assign({}, sorted, { included: [ ...sorted.included, name ] })
-        : Object.assign({}, sorted, { alternative: [ ...sorted.alternative, name ] }),
-    { included: [], alternative: [] }
-  )
-  console.log(`Todays names: ${included.join(`, `)}`)
-  alternative && console.log(`Alternative names: ${alternative.join(`, `)}`)
-})
+commander
+  .command(`today`)
+  .option(`-a, --all,`, `Include deprecated names`)
+  .action(command => {
+    const names = lookupNameday(new Date())
+    const { included, alternative } = names.reduce(
+      (sorted, { included, name }) =>
+        included
+          ? Object.assign({}, sorted, { included: [ ...sorted.included, name ] })
+          : Object.assign({}, sorted, {
+            alternative: [ ...sorted.alternative, name ]
+          }),
+      { included: [], alternative: [] }
+    )
+
+    const defaultOutput = `Todays names: ${included.join(`, `)}`
+
+    command.all
+      ? log(defaultOutput.concat(` (Alternative: ${alternative.join(`, `)})`))
+      : log(defaultOutput)
+  })
 
 process.argv.slice(2).length === 0
   ? commander.help()
