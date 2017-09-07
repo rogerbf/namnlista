ebfc5ae85fc74e7ae514f0fa51cc7ad79f4fe0a4
rogerbf <roger@bergfalk.nu>
2017-09-07 23:28:48 +0200

    export lookup function, all names, external providers

--- source/main.js
+++ source/main.js
@@ -1,3 +1,7 @@
-import akademien from 'akademien'
+import akademien from "akademien"
+import * as external from "./external/external"
 
-export default akademien.namedays.lookup
+export default Object.assign(input => akademien.namedays.lookup(input), {
+  names: akademien.namedays.names,
+  external
+})
